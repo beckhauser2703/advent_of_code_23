735f080a76e2cf87be96a17b7edb434c208e4e0c
Antônio Augusto Beckhauser Francisco <beckhauser2703@gmail.com>
2023-12-08 14:24:39 -0300

    mudando para as convenções de nome do sonar

--- day_8/solution.py
+++ day_8/solution.py
@@ -13,7 +13,7 @@ def parse_input(input: str) -> Tuple[str, Dict[str, List[str]]]:
     return instructions, network_dictionary
 
 
-def number_of_steps_until_ZZZ(current_node: str, endpoints: List[str], directions: str, network: Dict[str, List[str]]):
+def number_of_steps_until_zzz(current_node: str, endpoints: List[str], directions: str, network: Dict[str, List[str]]):
     counter = 0
     while (current_node not in endpoints):
         counter += 1
@@ -26,7 +26,7 @@ def number_of_steps_until_ZZZ(current_node: str, endpoints: List[str], direction
 def solution_part_1(input: str) -> None:
     directions, network_dict = parse_input(input)
     current_node = 'AAA'
-    print(number_of_steps_until_ZZZ(
+    print(number_of_steps_until_zzz(
         current_node, ['ZZZ'], directions, network_dict))
 
 
@@ -34,9 +34,9 @@ def solution_part_2(input: str) -> None:
     from math import lcm
     directions, network_dict = parse_input(input)
     endpoint_nodes = [node for node in network_dict.keys() if node[-1] == 'Z']
-    array_steps_to_XXZ = [number_of_steps_until_ZZZ(node, endpoint_nodes, directions, network_dict)
+    array_steps_to_xxz = [number_of_steps_until_zzz(node, endpoint_nodes, directions, network_dict)
                           for node in network_dict.keys() if node[-1] == 'A']
-    print(lcm(*array_steps_to_XXZ))
+    print(lcm(*array_steps_to_xxz))
 
 
 with open(r'day_8\puzzle_input.txt', 'r') as input:
