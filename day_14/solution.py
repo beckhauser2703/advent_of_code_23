42527aecbefacebfb7f846a1c95707ae70d6f291
Antônio Augusto Beckhauser Francisco <beckhauser2703@gmail.com>
2023-12-14 13:39:43 -0300

    refatorando parte 1

--- day_14/solution.py
+++ day_14/solution.py
@@ -1,25 +1,28 @@
 from typing import List, Tuple, Dict
 
-def move_north(grid: List[List[str]]) -> None:
-    return
+def move_col_north(grid: List[List[str]], col: int) -> int:
+    last_obstacle_row = -1
+    answer = 0
+    for row in range(len(grid)):
+        current_item = grid[row][col]
+        if current_item == '.':
+            continue
+        if current_item == '#':
+            last_obstacle_row = row
+            continue
+        #item can only be O
+        grid[row][col] = '.'
+        last_obstacle_row += 1
+        grid[last_obstacle_row][col] = 'O'
+        answer += (len(grid) - last_obstacle_row)
+    return answer
+
 
 def solution_part_1(input: str) -> None:
     split_input = [[char for char in line] for line in input.split("\n")]
     answer = 0
     for col in range(len(split_input[0])):
-        last_obstacle_row = -1
-        for row in range(len(split_input)):
-            current_item = split_input[row][col]
-            if current_item == '.':
-                continue
-            if current_item == '#':
-                last_obstacle_row = row
-                continue
-            #item can only be O
-            split_input[row][col] = '.'
-            last_obstacle_row += 1
-            split_input[last_obstacle_row][col] = 'O'
-            answer += (len(split_input) - last_obstacle_row)
+        answer += move_col_north(split_input, col)
     print(answer)
 
 
